98ab77d2b3317e9cd0a89bf6cfb15d1f8c280e10
majid <smkh1985@gmail.com>
2020-08-20 23:20:51 +0430

    Create datapoint and plot them

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ from matplotlib import pyplot as plt
 
 def main():
     print('This code is written by SMKH1985')
-
+    x = np.arange(0,10,0.1)
+    y = np.sin(x)
+    plt.plot(x,y)
+    plt.show()
 
 if __name__ == "__main__":
     main()
