8184dad6203f82c9d179c066d0b8444548f55226
majid <smkh1985@gmail.com>
2020-08-20 23:12:15 +0430

    Write main function

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ import pandas as pd
 import numpy as np 
 
 from matplotlib import pyplot as plt 
+
+
+def main():
+    print('This code is written by SMKH1985')
+
+
+if __name__ == "__main__":
+    main()
